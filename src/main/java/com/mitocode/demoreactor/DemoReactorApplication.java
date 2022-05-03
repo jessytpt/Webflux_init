bbe48149bc4027975dac55c0fb9dbef5e6f01d66
Jessenia Toapanta <jessytpt.dev@gmail.com>
2022-05-03 10:57:34 -0500

    trandformado de flux a mono

--- src/main/java/com/mitocode/demoreactor/DemoReactorApplication.java
+++ src/main/java/com/mitocode/demoreactor/DemoReactorApplication.java
@@ -9,7 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoReactorApplication implements CommandLineRunner {
@@ -18,13 +22,45 @@ public class DemoReactorApplication implements CommandLineRunner {
 
 	public void reactor(){
 		Mono.just(new Persona(1,"Mito",10))
+				.doOnNext(p -> {
+					//lógica adicional
+					log.info("[Reactor] Persona: "+ p);
+				})
 				.subscribe(p -> log.info("[Reactor] Persona: "+ p));
 	}
 
 	public void rxjava2(){
 		Observable.just(new Persona(1,"Mito",10))
+				.doOnNext(p -> log.info("[Reactor] Persona: "+ p))
 				.subscribe(p -> log.info("[RxJava2] Persona: "+ p));
 	}
+
+	public void mono(){
+		Mono.just(new Persona(1,"Mito",10))
+				.subscribe(p-> log.info(p.toString()));
+	}
+
+	public void flux(){
+		List<Persona> personas = new ArrayList<>();
+		personas.add(new Persona(1,"Jessenia",27));
+		personas.add(new Persona(2,"Juan Pablo",23));
+		personas.add(new Persona(3,"Miguel",29));
+
+		Flux.fromIterable(personas).subscribe(p -> log.info(p.toString()));
+	}
+
+	//Convertir un flux en mono
+public void fluxMono(){
+	List<Persona> personas = new ArrayList<>();
+	personas.add(new Persona(1,"Jessenia",27));
+	personas.add(new Persona(2,"Juan Pablo",23));
+	personas.add(new Persona(3,"Miguel",29));
+
+	Flux<Persona> fx = Flux.fromIterable(personas);
+	fx.collectList().subscribe(lista -> log.info(lista.toString()));
+
+	}
+
 
 
 	public static void main(String[] args) {
@@ -33,7 +69,8 @@ public class DemoReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		reactor();
-		rxjava2();
+		//mono();
+		//flux();
+		fluxMono();
 	}
 }
